4c391badc963010ac8c30e6bd9d18723fe91770e
Kevin Chaparro <kevinchaparro@ElKevin2.local>
2025-01-30 18:17:37 -0700

    Fix:Se termina modulo User con sus validciones

--- task-manager-api_nestjs/src/users/dto/create-user.dto.ts
+++ task-manager-api_nestjs/src/users/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class CreateUserDto {  
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+  
+    @IsString()
+    @IsNotEmpty()
+    email: string;
+  
+    @IsString()
+    @IsNotEmpty()
+    user: string;
+  
+    @IsNumber()
+    @IsNotEmpty()
+    status: number;
+}

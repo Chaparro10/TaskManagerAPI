2eee2d3a347e681a225961187462ff4f55c77c01
Kevin Chaparro <kevinchaparro@Sistemas-Castelo.grupocalzzapato.com.mx>
2025-02-27 11:18:37 -0700

    Fix:eliminacion

--- TASKMANAGERAPI_EXPRESS/src/config.js
+++ TASKMANAGERAPI_EXPRESS/src/config.js
@@ -1,9 +1,9 @@
 
-export const DB_HOST=process.env.DB_HOST || 'localhost';
-export const DB_PORT=process.env.DB_PORT || 3306;
-export const DB_USER=process.env.DB_USER || 'root';
-export const DB_PASS=process.env.DB_PASS || 'Chaparro07';
-export const DB_NAME=process.env.DB_NAME || 'Control_Almacen';
-export const MONGO_URI=process.env.MONGO_URI || 'mongodb://localhost:27017';
+export const DB_HOST = process.env.DB_HOST;
+export const DB_PORT = process.env.DB_PORT;
+export const DB_USER = process.env.DB_USER;
+export const DB_PASS = process.env.DB_PASS;
+export const DB_NAME = process.env.DB_NAME;
+export const MONGO_URI = process.env.MONGO_URI;
 
 
